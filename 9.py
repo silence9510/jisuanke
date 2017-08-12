9ed940d132ebb5645b6a0dd4f7286669a4be7f0a
silence9510 <1016186751@qq.com>
2017-08-12 11:58:27 +0800

    单独的数字

---
+++ 9.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'zhangchengliang'
+
+def is_ele(num):
+    return num != ele
+
+n = int(input())
+arr = map(int, input().split())
+ele = int(input())
+
+print(len(list(filter(is_ele, arr))))
