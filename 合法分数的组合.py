cc17c3d52fa98f945a4b10f42c3f569f132346d5
silence9510 <1016186751@qq.com>
2017-08-30 21:48:09 +0800

    水水更健康

---
+++ 合法分数的组合.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#description:
+__author__ = 'zhangchengliang'
+
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+    return a
+
+n = int(input())
+
+arr = [(i,j) for i in range(n+1) for j in range(1, n+1) if gcd(i, j)==1 if i <= j ]
+arr = sorted(arr, key=lambda ele : float(ele[0] / ele[1]))
+for ele in arr:
+    print("{0}/{1}".format(ele[0], ele[1]))
